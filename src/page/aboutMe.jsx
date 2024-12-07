7e4410ed5234edf72826f53bde2306947e0a5215
Ben <benmcmillen12@gmail.com>
2024-12-07 21:17:55 +1100

    fix

--- src/page/aboutMe.jsx
+++ src/page/aboutMe.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { StyledButton,
   StyledHr, MainHeading, CircleImage, InvertStyledButton} from '../styles/styledComponents';
-
+import im1 from 'src/assets/ProfilePicture.jpg';
+import im2 from 'src/assets/icons8-linkedin.svg';
 const AboutMe = function({ }) {
   // Initilise local varaibles
   const navigate = useNavigate();
@@ -12,11 +13,11 @@ const AboutMe = function({ }) {
     <>
       <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', flexWrap: 'wrap'}}>
         <div style={{display: 'flex', flexDirection:'column', textAlign: 'center', alignContent: 'center', alignItems: 'center', paddingLeft: '25%', paddingRight: '25%', gap:'10px'}}>
-          <CircleImage src='src/assets/ProfilePicture.jpg' alt='Profile Picture'/>
+          <CircleImage src={im1} alt='Profile Picture'/>
           <MainHeading>About Me</MainHeading>
           <div style={{color: 'black'}}> Hello! My name is Ben, and I’m a final-year Mechatronics Engineering and Computer Science student at UNSW. Currently, I am a Casual Academic at UNSW, where I have the opportunity to work closely with students, as well as an Embedded Systems Engineering Intern at Endeavour Aerospace. I am passionate about robotics and embedded systems and enjoy working on projects that blend innovation with practical solutions.</div>
           <a href="https://www.linkedin.com/in/ben-mcmillen-b587b7227/" target="_blank" rel="noopener noreferrer">
-            <img src="src/assets/icons8-linkedin.svg" alt="LinkedIn" />
+            <img src={im2} alt="LinkedIn" />
           </a>
           <div style={{display: 'flex', gap:'5px'}}>
             <StyledButton onClick={() =>{navigate('/experience')}}>View my Experience</StyledButton>
--- src/page/projects.jsx
+++ src/page/projects.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { StyledButton, ProjectBox,SquareImage, InnerText, InnerTitle,
   StyledHr, MainHeading, MainHeading2} from '../styles/styledComponents';
-
+import im1 from '../assets/handGestureRobot.png';
+import im2 from '../assets/sim.png';
+import im3 from '../assets/mergeSortVisHalf.png';
+import im4 from '../assets/mazeGettingSolved.png';
+import im5 from '../assets/webpage.png';
 const Projects = function({ }) {
   // Initilise local varaibles
   const navigate = useNavigate();
@@ -16,35 +20,35 @@ const Projects = function({ }) {
     <StyledHr/>
     <div style={{display: 'flex', flexDirection: 'column', width:'100%', justifyContent: 'flex-start'}}>
       <ProjectBox onClick={() => {navigate('/projects/handgesturerobot')}}>
-        <SquareImage src='src/assets/handGestureRobot.png'/>
+        <SquareImage src={im1}/>
         <div style={{display: 'flex',flexDirection:'column', justifyContent: 'flex-start', overflow: 'hidden', maxWidth:'800px'}}>
             <MainHeading2>Hand Gesture Robot</MainHeading2>
             <InnerText>This project involves the development of a hand gesture-controlled robot, utilizing both Arduino and Python programming... </InnerText>
         </div>
       </ProjectBox>
       <ProjectBox onClick={() => {navigate('/projects/robotimplementation')}}>
-        <SquareImage src='src/assets/sim.png'/>
+        <SquareImage src={im2}/>
         <div style={{display: 'flex',flexDirection:'column', justifyContent: 'flex-start', overflow: 'hidden', maxWidth:'800px'}}>
             <MainHeading2>Robot Implementation</MainHeading2>
             <InnerText>This project is a robotic car system designed to showcase advanced programming concepts and efficient code organization... </InnerText>
         </div>
       </ProjectBox>
       <ProjectBox onClick={() => {navigate('/projects/mergesortvis')}}>
-        <SquareImage src='src/assets/mergeSortVisHalf.png'/>
+        <SquareImage src={im3}/>
         <div style={{display: 'flex',flexDirection:'column', justifyContent: 'flex-start',maxWidth:'800px'}}>
           <MainHeading2>Merge Sort Visualizer</MainHeading2>
           <InnerText>This program is a Simple Merge Sort Visualizer. It acts to both generate 100 random numbers ranging from 1-100 using the provided "Generate" button and then sorts these numbers using merge sort using the "Sort" button... </InnerText>
         </div>
       </ProjectBox>
       <ProjectBox onClick={() => {navigate('/projects/mazesolver')}}>
-        <SquareImage src='src/assets/mazeGettingSolved.png'/>
+        <SquareImage src={im4}/>
         <div style={{display: 'flex',flexDirection:'column', justifyContent: 'flex-start',maxWidth:'800px'}}>
           <MainHeading2>Maze Solver</MainHeading2>
           <InnerText>This project gives the user the ability to design their own maze by creating a text file in the maze folder. Also providing a visulation of how these mazes are solved... </InnerText>
         </div>
       </ProjectBox>
       <ProjectBox onClick={() => {navigate('/projects/webpage')}}>
-        <SquareImage src='src/assets/webpage.png'/>
+        <SquareImage src={im5}/>
         <div style={{display: 'flex',flexDirection:'column', justifyContent: 'flex-start',maxWidth:'800px'}}>
           <MainHeading2>This WebSite</MainHeading2>
           <InnerText> This website was designed as a way to highlight both my experience and projects completed... </InnerText>
